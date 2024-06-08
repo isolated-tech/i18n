3119978002090f10c7c3d2e2d7eeaeb6a087646f
Cody Bontecou <bontecouc@gmail.com>
2024-06-08 17:03:01 +0800

    fileinput component

---
+++ components/FileInput.client.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const emit = defineEmits(['fileChange'])
+
+const handleFileChange = (event: Event) => {
+  const file = event.target.files[0]
+
+  const reader = new FileReader()
+  reader.onload = e => {
+    emit('fileChange', e.target.result)
+  }
+
+  reader.readAsText(file)
+}
+</script>
+
+<template>
+  <Input id="picture" type="file" accept="json" @change="handleFileChange" />
+</template>
