03bff1a99b020b1e836834262cee0aed16b04b44
Cody Bontecou <bontecouc@gmail.com>
2024-06-15 16:25:54 +0800

    tos + privacy pages

--- pages/privacy.client.vue
+++ pages/privacy.client.vue
@@ -1,3 +1,89 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
+</script>
+
 <template>
-  <h1>privacy</h1>
+  <div
+    class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 scroll-smooth"
+  >
+    <section
+      class="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:gap-x-8 lg:px-8 lg:py-40"
+    >
+      <div class="px-6 lg:px-0 lg:pt-4">
+        <div class="mx-auto max-w-2xl">
+          <div class="max-w-lg">
+            <NuxtLink to="/">
+              <ArrowLeftIcon class="h-11 cursor-pointer" />
+            </NuxtLink>
+            <h1
+              class="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+            >
+              Privacy Policy
+            </h1>
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              Your privacy is important to us. It is i18n AI's policy to respect
+              your privacy regarding any information we may collect from you
+              across our website, and other sites we own and operate.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              We only ask for personal information when we truly need it to
+              provide a service to you. We collect it by fair and lawful means,
+              with your knowledge and consent. We also let you know why we're
+              collecting it and how it will be used.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              You can sign up with your X or Github account so your i18n AI's
+              account email will be prefilled with the value they provide.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              We only retain collected information for as long as necessary to
+              provide you with your requested service. What data we store, we'll
+              protect within commercially acceptable means to prevent loss and
+              theft, as well as unauthorized access, disclosure, copying, use or
+              modification.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              We don't share any personally identifying information publicly or
+              with third-parties, except when required to by law.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              We act in the capacity of a data controller and a data processor
+              with regard to the personal data processed through ShipFast and
+              the services in terms of the applicable data protection laws,
+              including the EU General Data Protection Regulation (GDPR).
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              Our website may link to external sites that are not operated by
+              us. Please be aware that we have no control over the content and
+              practices of these sites, and cannot accept responsibility or
+              liability for their respective privacy policies.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              You are free to refuse our request for your personal information,
+              with the understanding that we may be unable to provide you with
+              some of your desired services.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              Your continued use of our website will be regarded as acceptance
+              of our practices around privacy and personal information. If you
+              have any questions about how we handle user data and personal
+              information, feel free to contact us.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              This policy is effective as of 15 June 2024.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
--- pages/tos.client.vue
+++ pages/tos.client.vue
@@ -1,3 +1,128 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
+</script>
+
 <template>
-  <h1>tos</h1>
+  <div
+    class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 scroll-smooth"
+  >
+    <section
+      class="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:gap-x-8 lg:px-8 lg:py-40"
+    >
+      <div class="px-6 lg:px-0 lg:pt-4">
+        <div class="mx-auto max-w-2xl">
+          <div class="max-w-lg">
+            <NuxtLink to="/">
+              <ArrowLeftIcon class="h-11 cursor-pointer" />
+            </NuxtLink>
+            <h1
+              class="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+            >
+              Terms of Service
+            </h1>
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">1. Introduction:</span> By using i18n you
+              confirm your acceptance of, and agree to be bound by, these terms
+              and conditions.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold"
+                >2. Agreement to Terms and Conditions:</span
+              >
+              This Agreement takes effect on the date on which you first use the
+              i18n application.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold"
+                >3. Unlimited Access Software License with Termination
+                Rights:</span
+              >
+              The i18n Software License facilitates the acquisition of i18n
+              software through a single purchase, granting users unrestricted
+              and perpetual access to its comprehensive functionalities.
+              Tailored for independent creators, entrepreneurs, and small
+              businesses, i18n empowers users to create compelling web pages and
+              online portfolios. This license entails a straightforward and
+              flexible arrangement, exempting users from recurring fees or
+              subscriptions. However, it is important to acknowledge that the
+              licensor retains the right to terminate the license without
+              conditions or prerequisites. This termination provision enables
+              the licensor to exercise control over software distribution and
+              utilization. Opting for the i18n Software License enables users to
+              enjoy the benefits of the software while recognizing the
+              licensor's unrestricted termination rights, which provide
+              adaptability and address potential unforeseen circumstances.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">4. Refunds:</span> Due to the nature of
+              digital products, the i18n boilerplate cannot be refunded or
+              exchanged once access is granted.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">5. Disclaimer:</span> It is not warranted
+              that i18n will meet your requirements or that its operation will
+              be uninterrupted or error free. All express and implied warranties
+              or conditions not stated in this Agreement (including without
+              limitation, loss of profits, loss or corruption of data, business
+              interruption or loss of contracts), so far as such exclusion or
+              disclaimer is permitted under the applicable law are excluded and
+              expressly disclaimed. This Agreement does not affect your
+              statutory rights.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold"
+                >6. Warranties and Limitation of Liability:</span
+              >
+              i18n does not give any warranty, guarantee or other term as to the
+              quality, fitness for purpose or otherwise of the software. i18n
+              shall not be liable to you by reason of any representation (unless
+              fraudulent), or any implied warranty, condition or other term, or
+              any duty at common law, for any loss of profit or any indirect,
+              special or consequential loss, damage, costs, expenses or other
+              claims (whether caused by i18n's negligence or the negligence of
+              its servants or agents or otherwise) which arise out of or in
+              connection with the provision of any goods or services by i18n.
+              i18n shall not be liable or deemed to be in breach of contract by
+              reason of any delay in performing, or failure to perform, any of
+              its obligations if the delay or failure was due to any cause
+              beyond its reasonable control. Notwithstanding contrary clauses in
+              this Agreement, in the event that i18n are deemed liable to you
+              for breach of this Agreement, you agree that i18n's liability is
+              limited to the amount actually paid by you for your services or
+              software, which amount calculated in reliance upon this clause.
+              You hereby release i18n from any and all obligations, liabilities
+              and claims in excess of this limitation.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">7. Responsibilities:</span> i18n is not
+              responsible for what the user does with the user-generated
+              content.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">8. General Terms and Law:</span> This
+              Agreement is governed by the laws of Singapore. You acknowledge
+              that no joint venture, partnership, employment, or agency
+              relationship exists between you and i18n as a result of your use
+              of these services. You agree not to hold yourself out as a
+              representative, agent or employee of i18n. You agree that i18n
+              will not be liable by reason of any representation, act or
+              omission to act by you.
+            </p>
+
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              <span class="font-bold">10. Effective Date:</span> This policy is
+              effective as of 15 June 2024.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
