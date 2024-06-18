d0a6b17b2ae15d6d63fe37e9470e6ae0a72453df
Cody Bontecou <bontecouc@gmail.com>
2024-06-18 18:53:26 +0800

    success/cancel

--- pages/cancelled.client.vue
+++ pages/cancelled.client.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/20/solid'
+</script>
+
 <template>
-  <h1>cancelled</h1>
+  <div
+    class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 scroll-smooth"
+  >
+    <section
+      class="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:gap-x-8 lg:px-8 lg:py-40"
+    >
+      <div class="px-6 lg:px-0 lg:pt-4">
+        <div class="mx-auto max-w-2xl">
+          <div class="max-w-lg">
+            <NuxtLink to="/">
+              <ArrowLeftIcon class="h-11 cursor-pointer" />
+            </NuxtLink>
+            <h1
+              class="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+            >
+              {{ $t('cancelled.hero') }}
+            </h1>
+            <p class="mt-5 text-lg leading-8 text-gray-600">
+              {{ $t('cancelled.description') }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
